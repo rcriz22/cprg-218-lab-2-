a3d60b620026704ad10f726b8910efa8238bd8e5
rcriz22 <raizelcriz.tayao@edu.sait.ca>
2024-11-06 17:01:36 -0700

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,19 @@
+sheep.addEventListener('click',function(){
+  document.getElementById('images').src="sheep.webp";
+});
+
+bear.addEventListener('click',function(){
+  document.getElementById('images').src="bear.webp";
+});
+
+moose.addEventListener('click',function(){
+  document.getElementById('images').src="moose.webp";
+});
+
+wolf.addEventListener('click',function(){
+  document.getElementById('images').src="wolf.webp";
+});
+
+elk.addEventListener('click',function(){
+  document.getElementById('images').src="elk.webp";
+});
